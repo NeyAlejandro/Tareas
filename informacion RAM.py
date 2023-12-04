512bc48cc38bd6b4dac149d739b9d64cacb08b0c
NeyAlejandro <135058337+NeyAlejandro@users.noreply.github.com>
2023-12-03 21:49:02 -0500

    Update informacion RAM.py

--- informacion RAM.py
+++ informacion RAM.py
@@ -1,23 +1,13 @@
-#IMPORTAMOS LIBRERIAS.
 import psutil
-import platform
-from datetime import datetime
 
-#FUNCION PARA REPRESENTAR TAMAÑOS EN BITS.
-def get_size(bytes, suffix="B"):
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
-#MEMORIA
-print("="*40, "Memory Information", "="*40)
-svmem = psutil.virtual_memory()
-print(f"Total: {get_size(svmem.total)}")
-print("="*20, "SWAP", "="*20)
-swap = psutil.swap_memory()
-print(f"Total: {get_size(swap.total)}")
-print(f"Free: {get_size(swap.free)}")
-print(f"Used: {get_size(swap.used)}")
-print(f"Percentage: {swap.percent}%")
+def mostrar_rendimiento_memoria():
+    memoria = psutil.virtual_memory()
 
+    print("Rendimiento de la memoria:")
+    print(f"  Total: {memoria.total / (1024 ** 3):.2f} GB")
+    print(f"  Disponible: {memoria.available / (1024 ** 3):.2f} GB")
+    print(f"  Usado: {memoria.used / (1024 ** 3):.2f} GB")
+    print(f"  Porcentaje de uso: {memoria.percent}%")
+
+if __name__ == "__main__":
+    mostrar_rendimiento_memoria()
